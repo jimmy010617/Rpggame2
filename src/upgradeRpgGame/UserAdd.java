d2066ee3af3ebdb6a1a9a647bb959512a56f23e8
jimmy010617 <jimmy990411@email.daelim.ac.kr>
2024-04-27 18:30:09 +0900

    게임1

---
+++ src/upgradeRpgGame/UserAdd.java
@@ -0,0 +1,19 @@
+package upgradeRpgGame;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.Reader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+import java.util.Set;
+
+public class UserAdd {
+    public static void account() throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        Reader fr = new FileReader("user.txt");
+    }
+}
