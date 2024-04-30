600781be54e7af92b84f2bf6822dd6b1b84da0e5
jimmy010617 <jimmy990411@email.daelim.ac.kr>
2024-04-30 15:01:44 +0900

    도적

---
+++ src/upgradeRpgGame/Thief.java
@@ -0,0 +1,71 @@
+package upgradeRpgGame;
+
+public class Thief extends Hero {
+    // 능력치 초기값
+    private int power = 0;
+    private int MP = 0;
+    private int dex = 10;
+    private int HP = 100;
+    private int Money = 1000;
+
+    public int getMoney() {
+        return Money;
+    }
+
+    public void setMoney(int money) {
+        if(money >= 0) {
+            this.Money = money;
+        }
+
+    }
+
+    public int getHP() {
+        return HP;
+    }
+
+    public void setHP(int Hp) {
+        HP = Hp;
+    }
+
+    private String name = "도적";
+
+    public int getPower() {
+        return power;
+    }
+
+    public void serPower(int power) {
+        this.power = power;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMP() {
+        return MP;
+    }
+
+    public void setMP(int Mp) {
+        this.MP = Mp;
+    }
+
+    public int getDex() {
+        return dex;
+    }
+
+    public void setDex(int dex) {   //능력치 최소 보장
+        if(dex >= 2) {
+            this.dex = dex;
+        }
+    }
+
+
+    @Override
+    void Attach() {
+        System.out.println("공격발동 : 럭키세븐");
+    }
+}
